abf71a8014a74e653a90b619c4ed34fcd7a6d927
Quoc Hai <quochaii163@gmail.com>
2018-11-18 13:54:40 +0700

    Them class Nha cung cap

---
+++ NhaCC.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QuanLyCF
+{
+    class NhaCC
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        //constructor
+        public NhaCC(string id, string name, string address)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Address = address;
+        }
+    }
+}
